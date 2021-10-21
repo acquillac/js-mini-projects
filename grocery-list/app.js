4ca6ca0ece61eb01b5788ddc614a2a2b3b610bb7
acquillac <acquillaclarke@live.ca>
2021-10-21 15:10:17 -0600

    local storage

--- grocery-list/app.js
+++ grocery-list/app.js
@@ -3,16 +3,48 @@ const submit = document.querySelector('.submit');
 const listItems = document.querySelector('.list');
 const clear = document.querySelector('.clear-items');
 
-
+// adding new item
 submit.addEventListener('click', () => {
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(input.value))
-  listItems.appendChild(li);
-  input.value = '';
+  if (input.value === ''){
+    alert('invalid input');
+  }else{
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value))
+    listItems.appendChild(li);
+    toLocalStorage(li.innerHTML);
+    input.value = '';
+  }
 })
 
+// clearing all items
 clear.addEventListener('click', () => {
-  listItems.innerHTML = '';
+  localStorage.clear();
+  location.reload();
 })
 
+// storing items
+function toLocalStorage(input) {
+  let storage;
+  if (localStorage.getItem('key') === null) {
+    storage = [];
+  } else {
+    storage = JSON.parse(localStorage.getItem('key'));
+  }
+  storage.push(input);
+  localStorage.setItem('key', JSON.stringify(storage))
+}
 
+// getting stored items
+document.addEventListener('DOMContentLoaded', () => {
+  let storage;
+  if (localStorage.getItem('key') === null) {
+    storage = [];
+  }else{
+    storage = JSON.parse(localStorage.getItem('key'));
+  }
+  storage.forEach((item) => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item))
+    listItems.appendChild(li);
+  })
+})
